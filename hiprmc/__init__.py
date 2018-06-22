a0da6ebf1e91a508eae457b87aab63918181b774
cnmelton <cnmelton@lbl.gov>
2018-06-22 15:42:14 -0700

    Added first stage of temperature tuning.

--- hiprmc/__init__.py
+++ hiprmc/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy import fftpack
+from scipy import fftpack, optimize, special
 from operator import mul
 import matplotlib.pyplot as plt
 import progressbar
@@ -7,10 +7,8 @@ import progressbar
 def fourier_transform(m):
     return fftpack.fft2(m)
 
-
 def chi_square(ft_simulation, ft_image):
     return np.sum((np.square(abs(abs(ft_simulation) - abs(ft_image))) / abs(ft_image)))
-
 
 # def DFT_Matrix(x_old, y_old, x_new, y_new, F_old, simulated_image):
 #     before_row, before_column = F_old[x_old, :], F_old[:, y_old]
@@ -23,22 +21,27 @@ def chi_square(ft_simulation, ft_image):
 
 
 def Metropolis(x_old, y_old, x_new, y_new, old_point, new_point, delta_chi, simulated_image, T, acceptance, chi_old,
-               chi_new):
-    if delta_chi < 0:
-        simulated_image[x_old][y_old] = new_point
-        simulated_image[x_new][y_new] = old_point
-        acceptance += 1.0
-        chi_old = chi_new
+               chi_new, T_MAX):
+    if T <= 0:
+        simulated_image[x_old][y_old] = old_point
+        simulated_image[x_new][y_new] = new_point
     else:
-        b = np.exp(-delta_chi / T)
-        if b > np.random.rand():
+        if delta_chi < 0:
             simulated_image[x_old][y_old] = new_point
             simulated_image[x_new][y_new] = old_point
             acceptance += 1.0
             chi_old = chi_new
         else:
-            simulated_image[x_old][y_old] = old_point
-            simulated_image[x_new][y_new] = new_point
+            temperature = T
+            b = np.exp(-delta_chi / temperature)
+            if b > np.random.rand():
+                simulated_image[x_old][y_old] = new_point
+                simulated_image[x_new][y_new] = old_point
+                acceptance += 1.0
+                chi_old = chi_new
+            else:
+                simulated_image[x_old][y_old] = old_point
+                simulated_image[x_new][y_new] = new_point
     return acceptance, chi_old
 
 def random_initial(image:np.array):
@@ -47,9 +50,16 @@ def random_initial(image:np.array):
     initial2[: np.count_nonzero(image == 1)] = 1
     np.random.shuffle(initial2)
     initial = initial2.reshape(image.shape)
+    # initial_size = int(len(image) / 4)
+    # initial = np.zeros((initial_size, initial_size))
+    # initial2 = initial.reshape(mul(*initial.shape))
+    # initial2[: int(load*initial_size**2)] = 1
+    # np.random.shuffle(initial2)
+    # initial = initial2.reshape(initial.shape)
     return initial
 
-def rmc(image:np.array, T, initial:np.array=None):
+
+def rmc(image: np.array, T, T_MAX, N, initial: np.array = None):
     if initial is None:
         initial = random_initial(image)
 
@@ -57,12 +67,14 @@ def rmc(image:np.array, T, initial:np.array=None):
     F_image = fourier_transform(image)
     F_old = fourier_transform(simulated_image)
     chi_old = chi_square(F_old, F_image)
-    for _ in progressbar.progressbar(range(0, 30000)):
+    error, count, accept_rate, temperature = [], [], [], []
+    delta_chi = chi_old
+    t_step = 100.0
+    for t in progressbar.progressbar(range(0, 200)):
         move_count = 0.0
         acceptance = 0.0
-        for _ in range(np.count_nonzero(simulated_image == 1)):
+        for _ in range(2 * np.count_nonzero(simulated_image == 1)):
             move_count += 1
-            # before_move = simulated_image.copy()
             x_old = np.random.randint(0, image.shape[0])
             y_old = np.random.randint(0, image.shape[1])
             while simulated_image[x_old][y_old] != 1:
@@ -70,6 +82,9 @@ def rmc(image:np.array, T, initial:np.array=None):
                 y_old = np.random.randint(0, image.shape[1])
             x_new = np.random.randint(0, image.shape[0])
             y_new = np.random.randint(0, image.shape[1])
+            while np.sqrt((y_new - y_old) ** 2 + (x_new - x_old) ** 2) > 7:
+                x_new = np.random.randint(0, image.shape[0])
+                y_new = np.random.randint(0, image.shape[1])
             old_point = simulated_image[x_old][y_old]
             new_point = simulated_image[x_new][y_new]
             while new_point == old_point:
@@ -87,8 +102,81 @@ def rmc(image:np.array, T, initial:np.array=None):
             chi_new = chi_square(F_new, F_image)
             delta_chi = chi_new - chi_old
             acceptance, chi_old = Metropolis(x_old, y_old, x_new, y_new, old_point, new_point, delta_chi,
-                                             simulated_image, T, acceptance, chi_old, chi_new)
+                                             simulated_image, T, acceptance, chi_old, chi_new, T_MAX)
             F_old = fourier_transform(simulated_image)
-        T = T * 0.9998
+        error.append(chi_old)
+        count.append(t)
+        accept_rate.append(acceptance / move_count)
+        temperature.append(T)
+        T = T - t_step
+
+    def sigmoid(x, a, b):
+        return a * (1 - np.exp(-(x / b) ** 2))
+
+    fit_params, fit_params_error = optimize.curve_fit(sigmoid, temperature, accept_rate, p0=[0.6, T_MAX / 4])
+
+    temp_tstar = fit_params[1] * np.sqrt(-np.log(1 - 0.3 / fit_params[0]))
+    t_min = min(0.0, temp_tstar - T_MAX / 10.0)
+    t_step = 10.0
+    t_max = min(T_MAX, temp_tstar + T_MAX / 10.0)
+    T = float(int(t_max))
+
+    temperature2, accept_rate2 = [], []
+    # simulated_image = initial.copy()
+    for t in progressbar.progressbar(range(int(t_min), int(t_max), int(t_step))):
+        move_count = 0.0
+        acceptance = 0.0
+        for _ in range(2 * np.count_nonzero(simulated_image == 1)):
+            move_count += 1
+            x_old = np.random.randint(0, image.shape[0])
+            y_old = np.random.randint(0, image.shape[1])
+            while simulated_image[x_old][y_old] != 1:
+                x_old = np.random.randint(0, image.shape[0])
+                y_old = np.random.randint(0, image.shape[1])
+            x_new = np.random.randint(0, image.shape[0])
+            y_new = np.random.randint(0, image.shape[1])
+            while np.sqrt((y_new - y_old) ** 2 + (x_new - x_old) ** 2) > 7:
+                x_new = np.random.randint(0, image.shape[0])
+                y_new = np.random.randint(0, image.shape[1])
+            old_point = simulated_image[x_old][y_old]
+            new_point = simulated_image[x_new][y_new]
+            while new_point == old_point:
+                x_new = np.random.randint(0, image.shape[0])
+                y_new = np.random.randint(0, image.shape[1])
+                new_point = simulated_image[x_new][y_new]
+            simulated_image[x_old][y_old] = new_point
+            simulated_image[x_new][y_new] = old_point
+            old_array = np.zeros_like(image)
+            old_array[x_old][y_old] = 1
+            new_array = np.zeros_like(image)
+            new_array[x_new][y_new] = 1
+            # U = DFT_Matrix(x_old, y_old, x_new, y_new, F_old, simulated_image)
+            F_new = F_old - fourier_transform(old_array) + fourier_transform(new_array)
+            chi_new = chi_square(F_new, F_image)
+            delta_chi = chi_new - chi_old
+            acceptance, chi_old = Metropolis(x_old, y_old, x_new, y_new, old_point, new_point, delta_chi,
+                                             simulated_image, T, acceptance, chi_old, chi_new, T_MAX)
+            F_old = fourier_transform(simulated_image)
+
+        accept_rate2.append(acceptance / move_count)
+        temperature2.append(T)
+        T = T - t_step
+
+    fit_params, fit_params_error = optimize.curve_fit(sigmoid, temperature2, accept_rate2, p0=[0.6, T_MAX / 4])
+
+    temp_tstar = fit_params[1] * np.sqrt(-np.log(1 - 0.3 / fit_params[0]))
+
+    f = plt.figure()
+    f.add_subplot(1, 2, 1)
+    plt.plot(temperature2, accept_rate2, 'b--')
+    plt.plot(temperature2, sigmoid(temperature2, *fit_params), 'r-')
+    plt.ylabel('Acceptance Rate')
+    plt.xlabel('Temperature')
+    f.add_subplot(1, 2, 2)
+    plt.plot(count, error, 'k-')
+    plt.ylabel('Chi-Squared')
+    plt.xlabel('Monte Carlo Iteration')
+    plt.tight_layout()
+    plt.show()
 
     return simulated_image
--- hiprmc/tests/test_simple.py
+++ hiprmc/tests/test_simple.py
@@ -3,39 +3,50 @@ import numpy as np
 from scipy import misc
 import hiprmc
 from skimage.draw import polygon
+from operator import mul
 
 def test_simple():
-    N = 25  # size of image (NxN)
-    disc = np.zeros((N, N))
-    T = 1000
+    N = 50  # size of image (NxN)
+    initial_image = np.zeros((N, N))
+    T = 20000
+    T_MAX = T
+
     # This bit creates a disc to try and model via RMC
     for i in range(0, N):
         for j in range(0, N):
-            r = (i - N / 2) ** 2 + (j - N / 2) ** 2
-            if r < (N / 2 - 2) ** 2 and r > (N / 2 - 4) ** 2:
-                disc[i, j] = 1
+            r = np.sqrt((i - int(N / 2)) ** 2 + (j - int(N / 2)) ** 2)
+            if r <= 7:
+                initial_image[i, j] = 1
             else:
-                disc[i, j] = 0
+                initial_image[i, j] = 0
 
-    # r = np.array([1, 12, 18, 1])
-    # c = np.array([1, 17, 14, 1])
+    # initial_image = np.zeros((N,N))
+    # initial2 = initial_image.reshape(mul(*initial_image.shape))
+    # initial2[: num_particles] = 1
+    # np.random.shuffle(initial2)
+    # initial_image = initial2.reshape(initial_image.shape)
+
+    # r = np.array([1, 20, 20, 1, 1])
+    # c = np.array([1, 1, 5, 5, 1])
     # rr, cc = polygon(r, c)
-    # disc[rr, cc] = 1
+    # initial_image[rr, cc] = 1
+
+    load = np.count_nonzero(initial_image == 1) / (N * N)
 
     # picture = Image.open('saxs10_128.tiff')
     # disc = misc.imread('saxs10_128.tiff')
     # print(disc)
 
-    initial = hiprmc.random_initial(disc)
-    simulated_image = hiprmc.rmc(disc, T, initial=initial)
+    initial = hiprmc.random_initial(initial_image)
+    simulated_image = hiprmc.rmc(initial_image, T, T_MAX, N, initial=initial)
 
     f = plt.figure()
     f.add_subplot(1, 5, 1)
-    plt.imshow(disc, cmap='Greys', origin="lower")
+    plt.imshow(initial_image, cmap='Greys', origin="lower")
     plt.axis('off')
     plt.title("original image")
     f.add_subplot(1, 5, 2)
-    plt.imshow(np.log10(abs(hiprmc.fourier_transform(disc))), origin="lower")
+    plt.imshow(np.log10(abs(hiprmc.fourier_transform(initial_image))), origin="lower")
     plt.axis('off')
     plt.title("FFT of Image")
     f.add_subplot(1, 5, 3)
