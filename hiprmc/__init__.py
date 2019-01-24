fixed DFT matrix calculation

hiprmc/__init__.py
@@ -17,8 +17,8 @@ def DFT_Matrix(x_old, y_old, x_new, y_new, N):
     m = dft(N)
     before_row, before_column = m[x_old, :], m[:, y_old]
     after_row, after_column = m[x_new, :], m[:, y_new]
-    U_new = np.outer(after_column, after_row)
-    U_old = np.outer(before_column, before_row)
+    U_new = np.outer(after_row, after_column)
+    U_old = np.outer(before_row, before_column)
     U = U_new - U_old
     return U
 
@@ -55,7 +55,7 @@ def random_initial(image:np.array):
     return initial
 
 
-def rmc(image: np.array, T_MAX, N, mode, initial: np.array = None):
+def rmc(image: np.array, T_MAX, N, iterations, initial: np.array = None):
     # uncomment the below two lines if using temperature tuning
     # simulated_image, t_max, t_min = hiprmc.temp_tuning(image, T_MAX, N, initial = initial)
     # T = t_max
@@ -68,17 +68,22 @@ def rmc(image: np.array, T_MAX, N, mode, initial: np.array = None):
     t_min = 0.0001
     T = T_MAX
 
-    iterations = 1000
+    iterations = 2000
     t_step = np.exp((np.log(t_min) - np.log(T_MAX)) / iterations)
 
-    if mode == 'simulated_data':
-        F_image = hiprmc.fourier_transform(image)
-
+    F_image = hiprmc.fourier_transform(image)
     F_old = hiprmc.fourier_transform(simulated_image)
     chi_old = hiprmc.chi_square(F_old, F_image)
 
     accept_rate, temperature, error, iteration = [], [], [], []
     move_distance = int(N / 2)
+
+
+    particle_list = []
+    for i in range(0, np.count_nonzero(simulated_image == 1)):
+        unfolded_simulated_image = simulated_image.reshape(mul(*simulated_image.shape))
+        if unfolded_simulated_image[i]
+        particle_list.append()
 
     for t in progressbar.progressbar(range(0, iterations)):
         move_count = 0.0

hiprmc/tests/test_simple.py
@@ -5,8 +5,9 @@ from skimage.draw import polygon, circle
 from PIL import Image
 
 def test_simple():
-    N = 100  # size of image (NxN)
+    N = 20  # size of image (NxN)
     initial_image = np.zeros((N, N))
+    iterations = 2000
 
     # This bit creates a disc to try and model via RMC
     # for i in range(0, N):
@@ -30,14 +31,11 @@ def test_simple():
     # set load of particles
     # load = 0.05 * N * N
 
-    # declare whether data is simulated or real
-    mode = 'simulated_data'
-
     T = N  # temperature is on the same order as the image
     T_MAX = T
 
     initial = hiprmc.random_initial(initial_image)
-    simulated_image = hiprmc.rmc(initial_image, T_MAX, N, mode, initial=initial)
+    simulated_image = hiprmc.rmc(initial_image, T_MAX, N, iterations, initial=initial)
 
     f = plt.figure()
     f.add_subplot(1, 5, 1)